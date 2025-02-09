68954590846f3b54874fedbe694c89f5c4cc8b55
Psihys <bogdan.s.0474@gmail.com>
2025-02-09 12:42:56 +0100

    s

--- LIza-Projects/Shevchenko.less-4.html/index.js
+++ LIza-Projects/Shevchenko.less-4.html/index.js
@@ -1,17 +1,16 @@
-const answer = document.getElementById('answer');
-const button = document.querySelector('button');
+const answer = document.getElementById('answer')
+const button = document.querySelector('button')
 
 button.addEventListener('click', (e) => {
-    e.preventDefault();
-    let text = answer.value;
-    if(text === '') {
-        alert('Введіть текст');
-    } else if (text !== 'Орел'){
-        alert('Неправильна відповідь, спробуй ще!');
-    }
-    else {
-        alert('Правильна відповідь, молодець!');
-    }
+  e.preventDefault()
+  let text = answer.value
+  if (text === '') {
+    alert('Введіть текст')
+  } else if (text === 'Орел' || text === 'орел') {
+    alert('Правильна відповідь, молодець!')
+  } else {
+    alert('Неправильна відповідь, спробуй ще!')
+  }
 
-    answer.value = '';
-});
+  answer.value = ''
+})
