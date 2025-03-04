b3531d3a7c67370a5d831f9498e25a693df96df6
Psihys <bogdan.s.0474@gmail.com>
2025-03-04 18:50:35 +0100

    s

--- beautiful-form/index.js
+++ beautiful-form/index.js
@@ -6,17 +6,17 @@ const sighUpBlock = document.getElementById("sighUpBlock");
 
 signIn.addEventListener("click" , () => {
     sighUpBlock.classList.add("is-hidden");
-    sighUpBlock.classList.remove("show");
+    sighUpBlock.classList.remove('is-visible')
     
     sighInBlock.classList.remove("is-hidden");
-    sighInBlock.classList.add("show");
+    sighInBlock.classList.add("is-visible");
 })
 
 signUp.addEventListener("click" , () => {
     sighUpBlock.classList.remove("is-hidden");
-    sighUpBlock.classList.add("show");
+    sighUpBlock.classList.add('is-visible')
 
     sighInBlock.classList.add("is-hidden");
-    sighInBlock.classList.remove("show");
+    sighInBlock.classList.remove('is-visible')
    
 })
