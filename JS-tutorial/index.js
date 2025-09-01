475fc1459452ad4d36a36c2d96810edf1f627349
Psihys <bogdan.s.0474@gmail.com>
2025-09-01 12:26:48 +0200

    і

---
+++ JS-tutorial/index.js
@@ -0,0 +1,31 @@
+// const product = prompt('Enter the product to check the prijs')
+
+// const productPrice = document.getElementById("product_price")
+// let price
+
+// switch (product.toLowerCase()) {
+//   case 'apple':
+//     price = 10
+    
+//     break;
+
+//     case 'orange':
+//     price = 15
+
+//     break;
+
+//     case 'banana':
+//     price = 5
+   
+//     break;
+
+//     default:
+//         price = "Unknown product"
+// }
+
+// console.log(price)
+
+// productPrice.textContent = price
+
+
+
