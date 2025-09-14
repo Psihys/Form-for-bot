0bec4fe23273e26e964c8a796adf67868acb6110
Psihys <bogdan.s.0474@gmail.com>
2025-09-14 17:43:11 +0200

    s

--- JS-tutorial/index.js
+++ JS-tutorial/index.js
@@ -1,81 +1,89 @@
-console.log("hello world")
+console.log('hello world')
 
 const obj1 = {
-    name: "John",
-    age: 30,
-    city: "New York",
-    jobInfo:{
-        prevJob: "Teacher",
-        currentJob: "Engineer",
-        salaryInfo: {
-            prevSalary: 30000,
-            currentSalary: 50000
-        }
-    }
-} 
+  name: 'John',
+  age: 30,
+  city: 'New York',
+  jobInfo: {
+    prevJob: 'Teacher',
+    currentJob: 'Engineer',
+    salaryInfo: {
+      prevSalary: 30000,
+      currentSalary: 50000,
+    },
+  },
+}
 
 const obj2 = {
-    name: "Jane",
-    age: 25,
-    city: "New York",
-    jobInfo:{
-        prevJob: "Teacher",
-        currentJob: "Engineer",
-        salaryInfo:{
-            prevSalary: 30000,
-            currentSalary: 50000
-        } 
-    }
+  name: 'Jane',
+  age: 25,
+  city: 'New York',
+  jobInfo: {
+    prevJob: 'Teacher',
+    currentJob: 'Engineer',
+    salaryInfo: {
+      prevSalary: 30000,
+      currentSalary: 50000,
+    },
+  },
+  importantInfo: function(){
+    console.log(`Expected salary is ${this.jobInfo.salaryInfo.currentSalary}`)
+  }
 }
 
-const areObjectsEqual = (object1, object2) =>{
-    if(typeof object1 !== "object" || typeof object2 !== "object" || object1 === null || object2 === null){
-        return object1 === object2
-    }
+console.log(obj1)
+delete obj1.jobInfo.salaryInfo.currentSalary
+obj2.importantInfo()
 
-    const key1 = Object.keys(object1)
-    console.log(key1)
+const stringifyJSON = JSON.stringify(obj1)
+console.log(stringifyJSON)
 
-    const key2 = Object.keys(object2)
-    console.log(key2)
+// const areObjectsEqual = (object1, object2) =>{
+//     if(typeof object1 !== "object" || typeof object2 !== "object" || object1 === null || object2 === null){
+//         return object1 === object2
+//     }
 
-    if(key1.length !== key2.length){
-        return false
-    }
+//     const key1 = Object.keys(object1)
+//     console.log(key1)
 
-    for(let key of key1){
-        if(!key2.includes(key)){
-            return false
-        }
+//     const key2 = Object.keys(object2)
+//     console.log(key2)
 
-        if(!areObjectsEqual(object1[key], object2[key])){
-            return false
-        }
-    }
+//     if(key1.length !== key2.length){
+//         return false
+//     }
 
-    return true
-}
+//     for(let key of key1){
+//         if(!key2.includes(key)){
+//             return false
+//         }
 
-console.log(areObjectsEqual(obj1, obj2))
+//         if(!areObjectsEqual(object1[key], object2[key])){
+//             return false
+//         }
+//     }
 
+//     return true
+// }
 
-const showObjectKeys = (object) =>{
-    if( typeof object !== "object" || object === null){
-        return
-    }
+// console.log(areObjectsEqual(obj1, obj2))
 
-    const keys = Object.keys(object)
-    
+// const showObjectKeys = (object) =>{
+//     if( typeof object !== "object" || object === null){
+//         return
+//     }
 
-    for (let key of keys){
-        const value = object[key]
+//     const keys = Object.keys(object)
 
-        if(typeof value === "object" && value !== null){
-            showObjectKeys(value)
-        }else {
-            console.log(`${key}: ${value}`)
-        }
-    }
-}
+//     for (let key of keys){
+//         const value = object[key]
 
-showObjectKeys(obj1)
+//         if(typeof value === "object" && value !== null){
+//             showObjectKeys(value)
+//         }else {
+//             console.log(`${key}: ${value}`)
+//         }
+//     }
+// }
+
+// showObjectKeys(obj1)
